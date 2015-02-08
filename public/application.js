76b85bf7f076219907866f3a397bdd44703541f3
Leif Roschier <leif.roschier@iki.fi>
2015-02-08 11:48:16 +0200

    map now loads and test button

--- public/application.js
+++ public/application.js
@@ -3,11 +3,18 @@
 // Declare app level module which depends on views, and components
 // karttaApp is the application
 var mainApplicationModuleName = 'karttaApp';
-angular.module(mainApplicationModuleName, ['ngResource','ngRoute','karttaMain']).
+angular.module(mainApplicationModuleName, ['ngResource','ngRoute','uiGmapgoogle-maps','karttaMain']).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   otherwise({redirectTo: '/'});
-}]);
+}]).
+config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+});
 
 // Manually bootstrap the AngularJS application
 angular.element(document).ready(function() {
--- public/karttaMain/karttaMain.client.controller.js
+++ public/karttaMain/karttaMain.client.controller.js
@@ -1,9 +1,10 @@
 // Invoke 'strict' JavaScript mode
 'use strict';
 
+
 // Create  'karttaMain' controller
-angular.module('karttaMain').controller('karttaMainController', ['$scope', 'SocketIO',
-    function($scope, SocketIO) {
+angular.module('karttaMain').controller('karttaMainController', ['$scope', 'SocketIO','uiGmapGoogleMapApi',
+    function($scope, SocketIO,uiGmapGoogleMapApi) {
         // Store messages
         $scope.messages = [];
         
@@ -34,7 +35,19 @@ angular.module('karttaMain').controller('karttaMainController', ['$scope', 'Sock
         // Remove the event listener when the controller instance is destroyed
         $scope.$on('$destroy', function() {
             SocketIO.removeListener('karttaMessage');
-        })
+        });
+
+        // uiGmapGoogleMapApi is a promise.
+    	// The "then" callback function provides the google.maps object.
+        uiGmapGoogleMapApi.then(function(maps) {
+            $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+        	console.log('map: ', maps);
+			console.log("loaded googlemaps api");
+        });
+        // for test changing map position
+        $scope.testMap = function() {
+                $scope.map = { center: { latitude: 55, longitude: -73 }, zoom: 6 };
+        }
 
     }
 ]); 
